efdbfafe7a48b8a4ae65f5a2d8dd2ca27c035215
safaldev <safalchhetri205@gmail.com>
2025-12-11 14:11:17 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import './index.css'
 
 export const App = () => {
   return (
     <>
-      <h1 className="HeadingOne" >Hello World</h1>
-      <p>Welcome to my first react project after a long time in a w</p>
+      <h1 className="font-extrabold text-7xl" >Hello World</h1>
+      <p className="font-semibold text-lg bg-red-400">Welcome to my first react project after a long time in a w</p>
     </>
   );
 };
