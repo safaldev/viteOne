d31332e6bdcfe89196a3ec5776abc71fcbe676e5
safaldev <safalchhetri205@gmail.com>
2025-12-11 08:38:13 +0000

    Some Code Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 export const App = () => {
-  return <> 
-  <h1>Hello World</h1>
-  <p>Welcome to my first react project after a long time in a w</p>
-  </>
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Welcome to my first react project after a long time in a w</p>
+    </>
+  );
 };
-
